f81141f6236c200f008b63955765ddfae5c186e5
Samuel Medina <smedinaguti@alumni.unav.es>
2022-11-09 12:17:20 +0100

    'primer experimento'

---
+++ primer_experimento.py
@@ -0,0 +1,26 @@
+import krakenex
+import pandas as pd
+import matplotlib.pyplot as plt
+
+k = krakenex.API()
+k.load_key('kraken.key')
+
+pair = 'XETHZUSD'
+
+
+            
+
+ohlc = pd.DataFrame (k.query_public('OHLC', data = {'pair': pair, 'interval' : 1,'since': None})['result'][pair])
+
+ohlc.columns = [
+                'time', 'open', 'high', 'low', 'close',
+                'vwap', 'volume', 'count',]
+
+print(ohlc)                
+
+
+#df = pd.DataFrame(resp['result'][pair])
+#plt.bar(df[0], df[1])
+#plt.show()
+#clear
+# print(df)
